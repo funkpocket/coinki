c3462d607424ed8961eda084018622abaf155b92
Ryan Brown <funkpocket@hotmail.com>
2014-02-27 13:50:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coinki::Application.config.secret_key_base = 'a788cd484c85fbfc8745487f5b68c2631e0db76a743602721f591cda03d8da386185732b031636f562ae18af2d2a2a7009a62314ca89e9b62cfdfa1160bddaed'
