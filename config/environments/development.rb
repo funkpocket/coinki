cddf7ea61e4b3b62f8469ca423a5c6c15c5e47cf
Ryan Brown <funkpocket@hotmail.com>
2014-02-27 20:05:38 -0500

    Implemented Devise for Users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Coinki::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #required for Heroku
+  config.action_mailer.default_url_options = { :host => 'http://coinkidink.herokuapp.com/' }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Coinki::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #required for Heroku
+  config.action_mailer.default_url_options = { :host => 'http://coinkidink.herokuapp.com/' }
 end
