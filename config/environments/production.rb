acfca96b97b6c51d5e04006c6e663913f952647e
Ryan Brown <funkpocket@hotmail.com>
2014-03-01 10:24:59 -0500

    set S3 access key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Coinki::Application.configure do
 
   #required for Heroku
   config.action_mailer.default_url_options = { :host => 'http://coinkidink.herokuapp.com/' }
+
+# sets Paperclip to upload images to Amazon S3
+    config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
